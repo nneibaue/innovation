77dd9c6ccc4e2d55d2c35d6b748dfc990a235a29
Nathan Neibauer <nneibauer044@gmail.com>
2023-12-21 14:38:32 +0000

    namespace enum

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from pydantic import TypeAdapter, validate_call, AfterValidator
 from pathlib import Path
 import yaml
 import random
+import jinja2 as j2
 
 from models import CardSet, Card
 from typing import Annotated, List
@@ -20,3 +21,13 @@ def load_cards(d: str | Path, shuffle: bool = True) -> CardSet:
     if shuffle:
         random.shuffle(cards)
     return CardSet(cards)
+
+
+def create_card_enum(input_dir: str | Path):
+    cards = load_cards(input_dir)
+    cards.root.sort(key=lambda c: c.name.lower())
+    with open('card_model.pytemplate', 'r') as f:
+        template=j2.Template(f.read())
+
+    with open('cards.py', 'w') as f:
+        f.write(template.render(cards=cards, d=input_dir))
